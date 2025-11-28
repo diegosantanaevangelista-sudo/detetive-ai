f4fd0a9292da4299d584aea28779c4fe5c91e460
diegosantanaevangelista-sudo <diegosantanaevangelista@gmail.com>
2025-11-28 17:16:41 -0300

    Delete index.jsx

--- index.jsx
+++ index.jsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40, fontFamily: 'Arial' }}>
-      <h1>Bem-vindo ao DetetiveAI</h1>
-      <p>Aqui você vai enviar prints ou textos para análise psicológica.</p>
-    </div>
-  );
-}
+
